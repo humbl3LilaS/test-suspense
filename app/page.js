e4b712f1393f927489df8f905446e4e153f0d13e
humbl3LilaS <sabishinekobebe@gmail.com>
2024-11-04 20:40:28 +0630

    testing complete

--- app/page.js
+++ app/page.js
@@ -4,10 +4,10 @@ import {Suspense} from "react";
 export default function Home() {
     return (
         <section className={"p-6"}>
-            <h1 className={"text-2xl font-bold"}>Todos</h1>
-            {/*<Suspense fallback={<div>Loading...</div>}>*/}
-            {/*    <TodoList/>*/}
-            {/*</Suspense>*/}
+            <h1 className={"mb-8 text-2xl font-bold"}>Todos</h1>
+            <Suspense fallback={<div>Loading...</div>}>
+                <TodoList/>
+            </Suspense>
         </section>
     );
 }
--- components/TodoList.jsx
+++ components/TodoList.jsx
@@ -1,15 +1,20 @@
-"use server"
+"use server";
 const TodoList = async () => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-    const data = await response.json();
-
-    console.log(data);  // logs data on the server side during rendering
-
-    return (
-        <ul>
-            <li>{data.title}</li>
-        </ul>
-    );
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await response.json();
+  
+  console.log(data);  // logs data on the server side during rendering
+  
+  return (
+    <ul className={"w-full h-[43vh] overflow-y-scroll"}>
+      {data && data.map(item => <li key={item.id}
+                                    className={"flex items-center justify-between px-6 py-4 odd:bg-blue-400 even:bg-amber-300 rounded-lg mb-4 last:mb-0"}
+      >
+        <span>{item.title}</span>
+        <span>{item?.completed ? "Completed" : "Not completed"}</span>
+      </li>)}
+    </ul>
+  );
 };
 
 export default TodoList;
